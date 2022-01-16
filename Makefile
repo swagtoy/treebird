3fe82bd18e6da6c4f81632fd0ecfc22fe70c6fe6
Swagtoy <me@ow.swag.toys>
2022-01-16 22:43:17 +0000

    File to C converter

--- Makefile
+++ Makefile
@@ -3,14 +3,21 @@ CFLAGS = -Wall
 LDFLAGS = -lcurl
 SRC = $(wildcard src/*.c)
 OBJ = $(patsubst %.c,%.o,$(SRC))
-TARGET = ratfe
+DIST = dist/
+TARGET = ratfe.cgi
 
 all: $(TARGET)
 
-$(TARGET): $(OBJ)
-	$(CC) -o $(TARGET) $(LDFLAGS)
+$(TARGET): filec $(OBJ)
+	$(CC) -o $(DIST)$(TARGET) $(LDFLAGS) $(OBJ)
 
 $(OBJ): $(SRC)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+filec: src/file-to-c/main.o
+	$(CC) -o file-to-c $<
+
+src/file-to-c/main.o: src/file-to-c/main.c
 	$(CC) $(CFLAGS) -c $< -o $@
 
 .PHONY: all
---
+++ src/main.c
@@ -0,0 +1,24 @@
+/*
+ * RatFE - Lightweight frontend for Pleroma
+ * Copyright (C) 2022 Nekobit
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Content-type: text/html\r\n\r\n<h1>Release the rats</h1>");
+}
