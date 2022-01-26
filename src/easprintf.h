be565106e91006e9d5243db8a73f9c6e3ad5b87f
Swagtoy <me@ow.swag.toys>
2022-01-26 04:09:03 +0000

    easprintf files

---
+++ src/easprintf.h
@@ -0,0 +1,25 @@
+/*
+ * RatFE - Lightweight frontend for Pleroma
+ * Copyright (C) 2022 Nekobit
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef EASPRINTF_H
+#define EASPRINTF_H
+
+int evasprintf(char** ret, const char* format, va_list ap);
+int easprintf(char** ret, const char* format, ...);
+
+#endif // EASPRINTF
