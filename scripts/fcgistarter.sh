e8b3f07ba37ec2911f7bd241f0f286c4f392314b
Swagtoy <me@ow.swag.toys>
2022-03-23 04:01:09 +0000

    better startup script

--- scripts/fcgistarter.sh
+++ scripts/fcgistarter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 kill -9 $(pgrep treebird.cgi) > /dev/null 2>&1
-fcgistarter -c $PWD/../dist/treebird.cgi -p 4000 -N 1
+fcgistarter -c $(dirname $(readlink -f $0))/../dist/treebird.cgi -p 4000 -N 1
