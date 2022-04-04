5418b01fd44a1e0d70950ec54dda14074d0252e9
Swagtoy <me@ow.swag.toys>
2022-04-04 16:37:05 +0000

    Improve CSS

--- scripts/fcgistarter.sh
+++ scripts/fcgistarter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-kill -9 $(pgrep treebird.cgi) > /dev/null 2>&1
-fcgistarter -c $(dirname $(readlink -f $0))/../dist/treebird.cgi -p 4000 -N 1
+kill -9 $(pgrep treebird) > /dev/null 2>&1
+fcgistarter -c $(dirname $(readlink -f $0))/../treebird -p 4000 -N 1
